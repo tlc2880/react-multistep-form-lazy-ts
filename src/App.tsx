986245e5b95fd8a122689aa7fa937befc8c1df50
tlc2880 <tommylcao@gmail.com>
2023-09-29 05:31:33 -0700

    Make improvements to the address and form-wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -43,20 +43,9 @@ function App() {
   }
 
   return (
-    <div
-      style={{
-        position: "relative",
-        background: "white",
-        border: "1px solid black",
-        padding: "2rem",
-        margin: "1rem",
-        borderRadius: ".5rem",
-        fontFamily: "Arial",
-        maxWidth: "max-content",
-      }}
-    >
+    <div className="form flex-col">
       <form onSubmit={onSubmit}>
-        <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
+        <div style={{ position: "relative", top: ".5rem", right: ".5rem" }}>
           {currentStepIndex + 1} / {steps.length}
         </div>
         {step}
